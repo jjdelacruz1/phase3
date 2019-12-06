2bd31bc91dcc74e98d8d84b9d2b79c196671066f
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-12-05 21:29:08 -0600

    Get the circles to change the background color when clicked on.

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,19 +1,78 @@
 import React from 'react';
 import './GameBoard.css';
 
-const ROW_SIZE = 300;
-const WIDTH = 800;
-const HEIGHT = 600;
+// TODO: 
+// 1. make predictions by getting info from cycleColor function and putting it into an array.
 
 class GameBoard extends React.Component {  
-  render() {    
+  constructor(props) {
+    super(props)
+    this.state = {
+      secretCode: [],
+      colorPredictions: [],
+      circle1: 0,
+      circle2: 0,
+      circle3: 0,
+      circle4: 0,
+      prediction1: [],
+      prediction2: [],
+      prediction3: [],
+      prediction4: []
+    }
+  }
+
+  cycleColor = (circle, key) => {
+    let nextCircleIndex = circle + 1
+    if (nextCircleIndex === 7) {
+      nextCircleIndex = 0
+    }
+    
+    this.setState({
+      [key]: nextCircleIndex
+    })
+    console.log(this.state)
+  }
+
+  render() {
+    const colorOptions = [null, 'red', 'cyan', 'green', 'orange', 'magenta', 'blue']
+    let key = ""
     return (      
-      <div>        
-        <div className="Board"          
-          style={{ width: WIDTH, height: HEIGHT }}>        
+      <div className="row container">  
+        <div className="border">      
+          <label type="button" className="dot"
+            style={
+              {
+                backgroundColor: colorOptions[this.state.circle1]
+              }
+            } onClick={() => this.cycleColor(this.state.circle1, key="circle1")}></label>       
+          <label type="button" className="dot"
+            style={
+              {
+                backgroundColor: colorOptions[this.state.circle2]
+              }
+            } onClick={() => this.cycleColor(this.state.circle2, key="circle2")}></label> 
+          <label type="button" className="dot"
+            style={
+              {
+                backgroundColor: colorOptions[this.state.circle3]
+              }
+            } onClick={() => this.cycleColor(this.state.circle3, key="circle3")}></label> 
+          <label type="button" className="dot"
+            style={
+              {
+                backgroundColor: colorOptions[this.state.circle4]
+              }
+            } onClick={() => this.cycleColor(this.state.circle4, key="circle4")}></label> 
+          
+        </div>    
+        <div className="border">
+          <span className="dot"></span>         
+          <span className="dot"></span> 
+          <span className="dot"></span> 
+          <span className="dot"></span> 
         </div>
-        <p>This is a test</p>
-      </div>    
+        <button>Check!</button>
+      </div>
       );  
     }}
 
