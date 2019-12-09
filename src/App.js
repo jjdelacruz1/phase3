647342a0bb5e65e4c71779c016a2295c1bc0c24f
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-12-08 21:18:52 -0600

    Update app to be a color matching game vs mastermind for now due to simplicity.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render () {
     return (
       <div className="Title">
-        <h1>Mastermind Easy Game</h1>
+        <h1>Color Code Match Game</h1>
         <Secret />
         <GameBoard />
       </div>
